98d49cbf854cd710588989dc90a15f9a7b5be3e1
zeedtanue <zeedtanue@gmail.com>
2021-03-30 03:07:51 +0800

    Update demo-plugin.php

--- demo-plugin.php
+++ demo-plugin.php
@@ -13,11 +13,11 @@
  * @package           Plugin_Name
  *
  * @wordpress-plugin
- * Plugin Name:       Demo Plugin
+ * Plugin Name:       Otto International
  * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       This is the assignment 1 for otto international's interview process
  * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Author:            Tamzeed Hossain
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
